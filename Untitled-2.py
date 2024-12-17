c97db9370996f7e69ef4119228bb23db2cab1b8c
scafandr1mode <egorov.i.a.work@gmail.com>
2024-12-17 11:01:16 +0300

    lst

---
+++ Untitled-2.py
@@ -0,0 +1,40 @@
+# lst = [i for i in range(1, 10)]
+# print(lst)
+
+# lst = [i for i in range(1,6)]
+# print(lst)
+
+# lst = [i for i in range(1,6)]
+# lst.sort(reverse=True)
+# print(lst)
+
+#2
+# lst = [i + 2 for i in range(0, 6)]
+# print(lst)
+
+# lst = [i / 2 for i in range(4, 10)]
+# print(lst)
+
+# lst = [i + 10 for i in range(0, 8, 2)]
+# print(lst)
+
+
+
+#3
+# lst = [1, 2, 3, 4, 5]
+# lst = [i **2 for i in [1,2,3,4,5]]
+# print(lst)
+
+# lst = [1, 3, 5, 7, 9]
+# lst = [i *3 for i in [1, 3, 5, 7, 9]]
+# print(lst)
+
+#4
+# lst = [i for i in [1, 3, 5, 7, 9] if i % 2 != 0]
+# print(lst)
+
+# lst = [-6, -3, -1, 0, 2, 4]
+# lst = [i for i in [-6, -3, -1, 0, 2, 4] if i>=0]
+# print(lst)
+
+#4
