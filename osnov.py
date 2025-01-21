a6e32b3a0939e4392d4b236ec1daaa146413a122
scafandr1mode <egorov.i.a.work@gmail.com>
2025-01-21 11:05:52 +0300

    sec com

---
+++ osnov.py
@@ -0,0 +1,10 @@
+from pers import *
+from lab import *
+import time
+def start():
+    suj()
+    time.sleep(3)
+    l()
+    time.sleep(3)
+    bum()
+start()
---
+++ pers.py
@@ -0,0 +1,16 @@
+
+def elfs():
+    elf = 'Robin'
+    elf_chr='Родился и вырос на северном полюсе , работает на Деда Мороза'
+    elf_int = ['Кристал','Леденцовый меч','Инструкция к крисаллу']
+    print(elf,'\n',elf_chr,'\n',elf_int)
+def snow():
+    snow_boss = 'Gargantuar'
+    snow_boss_chr = 'Жуткий монстр созданный из снега и льда'
+    snow_boss_int = [' 3 Часть адронного коллайдера','Палица','Ключ']
+    print(snow_boss,'\n',snow_boss_chr,'\n',snow_boss_int)
+def moroz():
+    moroz = 'Дед мороз'
+    moroz_chr='Добрый и весёлый дедушка , который дарит праздник '
+    moroz_int=['2 чать адронного коллайдера','установка для кристалла']
+    print(moroz,'\n',moroz_chr,'\n',moroz_int)
